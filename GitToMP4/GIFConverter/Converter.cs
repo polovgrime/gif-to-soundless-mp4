e4216300852a67dc5bec9a4fe9b7ae45800a072b
polovgrime <polovgrime@gmail.com>
2023-01-04 18:16:23 +0300

    Initial use of ffmpeg for converting

--- GitToMP4/GIFConverter/Converter.cs
+++ GitToMP4/GIFConverter/Converter.cs
@@ -1,7 +1,33 @@
-﻿namespace GIFConverter
-{
-    public class Class1
-    {
+﻿using System.Diagnostics;
 
+namespace GIFConverter
+{
+    public class Converter
+    {
+        private readonly string arguments = string.Empty;
+        private readonly string ffmpegExecutable = "ffmpeg";
+
+        private const string ARGUMENTS_TEMPLATE = "-i {0} -pix_fmt yuv420p {1}.mp4";
+
+        public Converter(string ffmpegExecutable)
+        {
+            this.ffmpegExecutable = ffmpegExecutable;
+        }
+
+        public bool Convert(string source, string output)
+        {
+            var arguments = string.Format(ARGUMENTS_TEMPLATE, source, output);
+            try
+            {
+                using (var proc = Process.Start(ffmpegExecutable, arguments))
+                {
+                    return true;
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
+        }
     }
 }
